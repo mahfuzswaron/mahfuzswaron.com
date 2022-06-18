41e539a7b0ac270d32bbd00554673879bbbdb88c
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-19 01:32:54 +0600

    changes made

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -5,11 +5,11 @@ const Project = ({ project }) => {
     const { name, img, description } = project;
     const navigate = useNavigate();
     return (
-        <div class="card w-full bg-white border border-secondary shadow-lg rounded-lg">
+        <div class="card w-full bg-white border border-secondary shadow-lg rounded-lg relative">
             <figure class="px-5 pt-5">
                 <img src={img} alt="Shoes" class="rounded-lg border border-secondary" />
             </figure>
-            <div class="card-body p-5 items-center text-center">
+            <div class="card-body p-5 items-center text-center absolute opacity-0 hover:opacity-100 ">
                 <h2 class="card-title text-primary text-3xl">{name}</h2>
                 <p className='hover:text-primary text-lg font-sans'>{description}</p>
                 <div class="card-actions">
